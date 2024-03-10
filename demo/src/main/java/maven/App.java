7be808e95c88ca08d1be190ab38c87116c30f9a8
unknown <C00286249@setu.ie>
2024-03-10 17:05:29 +0000

    added gui buttons

--- demo/src/main/java/maven/App.java
+++ demo/src/main/java/maven/App.java
@@ -1,4 +1,5 @@
 package maven;
+import java.awt.Color;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,32 +10,38 @@ public static void main(String[] args) throws SQLException {
     //establish connection
     Connection connection = DatabaseConnection.getConnection();
 
-    // Call the readTable function from TableReader class
-    String[] text = TableReader.readTable(connection, "customer");
-    System.out.println(text[1]);
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println(text[3]);
+    JFrame frame =new JFrame();//creating instance of JFrame 
+    frame.setSize(1280,720); 
+    frame.getContentPane().setBackground(new Color(246, 255, 248)); // gen pane and Set background color 
+    frame.setLayout(null);  
     
-     
-    JFrame f=new JFrame();//creating instance of JFrame 
- 
-    /*        
-    JButton b=new JButton("click");//creating instance of JButton  
-    b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-            
-    f.add(b);//adding button in JFrame  
-            
-    
-    */
-    //text  
-    JTextArea area=new JTextArea(text[3]);  
-    area.setBounds(10,30, 1000,600);  
-    f.add(area);  
-    f.setSize(1280,720);  
-    f.setLayout(null);  
-    f.setVisible(true);
+
+    //button
+    JButton regButton = new JButton("Register");
+    regButton.setBackground(new Color(204, 227, 222)); //grey
+    int regCenter_X = (frame.getWidth()- 95) /2;
+    regButton.setBounds(regCenter_X, 150, 95, 30);
+    frame.add(regButton);   //add our button
+    frame.setVisible(true);
+
+    //register
+    JButton logButton = new JButton("Login");
+    logButton.setBackground(new Color(204, 227, 222)); //grey
+    int logCenter_X = (frame.getWidth()- 95) /2;
+    logButton.setBounds(logCenter_X, 210, 95, 30);
+    frame.add(logButton);   //add our button
+    frame.setVisible(true);
+
+    JButton guestButton = new JButton("Guest");
+    guestButton.setBounds(50,100,95,30);
+    guestButton.setBackground(new Color(204, 227, 222)); //grey
+    int guestCenter_X = (frame.getWidth()- 95) /2;
+    guestButton.setBounds(guestCenter_X, 270, 95, 30);
+    frame.add(guestButton);   //add our button
+    frame.setVisible(true);
+
+
+    //exit program when app is closed
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 }
 }  
