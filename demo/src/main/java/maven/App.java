be4a932af1e4e92d0cdbd2e9ea3fcf6087cb8646
unknown <C00286249@setu.ie>
2024-03-19 22:28:15 +0000

    buttons

--- demo/src/main/java/maven/App.java
+++ demo/src/main/java/maven/App.java
@@ -3,15 +3,18 @@ import java.awt.Color;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;  
 public class App {  
 public static void main(String[] args) throws SQLException {  
 
     //establish connection
-    Connection connection = DatabaseConnection.getConnection();
+    //Connection connection = DatabaseConnection.getConnection();
 
     JFrame frame =new JFrame();//creating instance of JFrame 
-    frame.setSize(1280,720); 
+    frame.setSize(1000,720); 
     frame.getContentPane().setBackground(new Color(246, 255, 248)); // gen pane and Set background color 
     frame.setLayout(null);  
     
@@ -19,23 +22,27 @@ public static void main(String[] args) throws SQLException {
     //button
     JButton regButton = new JButton("Register");
     regButton.setBackground(new Color(204, 227, 222)); //grey
-    int regCenter_X = (frame.getWidth()- 95) /2;
+    int regCenter_X = (frame.getWidth()- 95) /2;     //get center of screen
     regButton.setBounds(regCenter_X, 150, 95, 30);
     frame.add(regButton);   //add our button
     frame.setVisible(true);
+    
+    //event listener
+    //ButtonHandler handler = new ButtonHan
+
 
     //register
     JButton logButton = new JButton("Login");
     logButton.setBackground(new Color(204, 227, 222)); //grey
-    int logCenter_X = (frame.getWidth()- 95) /2;
-    logButton.setBounds(logCenter_X, 210, 95, 30);
+    int logCenter_X = (frame.getWidth()- 95) /2;    //get center of screen
+    logButton.setBounds(logCenter_X, 210, 95, 30);  
     frame.add(logButton);   //add our button
     frame.setVisible(true);
 
     JButton guestButton = new JButton("Guest");
     guestButton.setBounds(50,100,95,30);
     guestButton.setBackground(new Color(204, 227, 222)); //grey
-    int guestCenter_X = (frame.getWidth()- 95) /2;
+    int guestCenter_X = (frame.getWidth()- 95) /2;   //get center of screen
     guestButton.setBounds(guestCenter_X, 270, 95, 30);
     frame.add(guestButton);   //add our button
     frame.setVisible(true);
